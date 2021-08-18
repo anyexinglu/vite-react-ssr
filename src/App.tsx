69db04c01be4c172ead2f43144402923f6c95648
anyexinglu <1171009543@qq.com>
2021-08-18 12:12:04 +0800

    simple for mini-vite demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
 import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
-
-// Auto generates routes from files under ./pages
-// https://vitejs.dev/guide/features.html#glob-import
-const pages = import.meta.globEager("./pages/*.tsx");
-
-const routes = Object.keys(pages).map(path => {
-  const name = ((path || "").match(/\.\/pages\/(.*)\.tsx$/) || [])[1] || "";
-  return {
-    name,
-    path: name === "Home" ? "/" : `/${name.toLowerCase()}`,
-    component: pages[path].default,
-  };
-});
-
+import Hello from "./hello";
 export function App() {
   return (
-    <>
-      <nav>
-        <ul>
-          {routes.map(({ name, path }) => {
-            return (
-              <li key={path}>
-                <Link to={path}>{name}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-      <Switch>
-        {routes.map(({ path, component: RouteComp }) => {
-          return (
-            <Route key={path} path={path}>
-              <RouteComp />
-            </Route>
-          );
-        })}
-      </Switch>
-    </>
+    <div>
+      App
+      <Hello />
+    </div>
   );
 }
